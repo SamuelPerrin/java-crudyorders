daf92106fe4c8e6e7564242e02fc65bfebc17c41
SamuelPerrin <samwperrin@yahoo.com>
2021-03-17 16:41:20 -0500

    finish customer controller

--- src/main/java/com/lambda/orders/demo/controllers/CustomersController.java
+++ src/main/java/com/lambda/orders/demo/controllers/CustomersController.java
@@ -4,10 +4,14 @@ import com.lambda.orders.demo.models.Customer;
 import com.lambda.orders.demo.services.CustomerServices;
 import com.lambda.orders.demo.views.CustomerOrderCount;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -44,6 +48,37 @@ public class CustomersController {
         return new ResponseEntity<>(custOrderCounts, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/customer", produces = "application/json", consumes = "application/json")
-    public ResponseEntity<?>
+    @PostMapping(value = "/customer", consumes = "application/json")
+    public ResponseEntity<?> addNewCustomer(@Valid @RequestBody Customer customer) {
+        customer.setCustcode(0);
+        customerServices.save(customer);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{custcode}")
+                .buildAndExpand(customer.getCustcode())
+                .toUri();
+        responseHeaders.setLocation(newCustomerURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/customer/{custcode}", consumes = "application/json")
+    public ResponseEntity<?> replaceCustomerById(@Valid @RequestBody Customer customer, @PathVariable long custcode) {
+        customer.setCustcode(custcode);
+        customerServices.save(customer);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/customer/{custcode}", consumes = "application/json")
+    public ResponseEntity<?> updateCustomerById(@RequestBody Customer customer, @PathVariable long custcode) {
+        customerServices.update(custcode, customer);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/customer/{custcode}")
+    public ResponseEntity<?> deleteCustomerById(@PathVariable long custcode) {
+        customerServices.delete(custcode);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
